f28be4833e437ab39a3613397c7e26742bc0bf1d
Frank Cash <cfcash@mtu.edu>
2015-02-22 10:51:11 -0500

    more updates

--- chrome-extension/contentRun.js
+++ chrome-extension/contentRun.js
@@ -2,23 +2,24 @@ function accessUserInfo(){
   // Getting
   chrome.storage.local.get("user", function(data) {
     // Do something with data.key
+    if(data.user.steps < data.user.goals){
+      $('form').submit(function(e) {
+        e.preventDefault();
+      });
+
+      $('button').prop('disabled', true);
+      $('form').submit(function(e) {
+        return false;
+      });
+      $('button').click(function(e){
+        return false;
+      })
+      $('button[type=submit]').click(function(e) {e.preventDefault();});
+
+    }
     console.log("true");
+
   });
 }
 
-$('form').submit(function(e) {
-  e.preventDefault();
-});
-
-
-
-
-
-
-$('button').prop('disabled', true);
-$('form').submit(function(e) {
-  return false;
-});
-$('button').click(function(e){
-  return false;
-})
+accessUserInfo();
