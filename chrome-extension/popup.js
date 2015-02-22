920aa4def871cecab84397e21a5914c56c614562
Frank Cash <cfcash@mtu.edu>
2015-02-22 08:51:37 -0500

    Removes function I was referencing

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -9,50 +9,6 @@ function onPageDetailsReceived(pageDetails)  {
 // Global reference to the status display SPAN
 var statusDisplay = null;
 
-// POST the data to the server using XMLHttpRequest
-function addBookmark() {
-    // Cancel the form submit
-    event.preventDefault();
-
-    // The URL to POST our data to
-    var postUrl = 'http://post-test.local.com';
-
-    // Set up an asynchronous AJAX POST request
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', postUrl, true);
-
-    // Prepare the data to be POSTed by URLEncoding each field's contents
-    var username = encodeURIComponent(document.getElementById('username').value);
-
-    var params = 'title=' + title;
-
-    // Replace any instances of the URLEncoded space char with +
-    params = params.replace(/%20/g, '+');
-
-    // Set correct header for form data
-    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-
-    // Handle request state change events
-    xhr.onreadystatechange = function() {
-        // If the request completed
-        if (xhr.readyState == 4) {
-            statusDisplay.innerHTML = '';
-            if (xhr.status == 200) {
-                // If it was a success, close the popup after a short delay
-                statusDisplay.innerHTML = 'Saved!';
-                window.setTimeout(window.close, 1000);
-            } else {
-                // Show what went wrong
-                statusDisplay.innerHTML = 'Error saving: ' + xhr.statusText;
-            }
-        }
-    };
-
-    // Send the request and set status
-    xhr.send(params);
-    statusDisplay.innerHTML = 'Saving...';
-}
-
 
 function saveUserInfo(){
   event.preventDefault();
