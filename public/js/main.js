565c5785eb7361f552c280686abfdf9d5925fec8
Frank Cash <cfcash@mtu.edu>
2015-02-22 02:25:25 -0500

    Works around oauth call back get tokens to angular
    this will allow association between an account and access token and
    secret.
    TODO: finish __hack__
    TODO: add encryption
    TODO: add password

--- public/js/main.js
+++ public/js/main.js
@@ -4,25 +4,27 @@
   app.config(['$routeProvider', function($routeProvider) {
     $routeProvider
     .when('/', {templateUrl: '/home.html',   controller: 'IndexCtrl'})
-    .when('/configure', {templateUrl: '/configure.html',   controller: 'ConfigureCtrl'})
+    .when('/configure/:token/:secret', {templateUrl: '/configure.html',   controller: 'ConfigureCtrl'})
     .otherwise({redirectTo: '/'});
   }]);
 
   app.controller('IndexCtrl', ['$scope', '$route','$routeParams',
   function($scope, $route, $routeParams){
     console.log("route", $route);
-
     console.log("IndexCtrl");
 
   }]);
 
   app.controller('ConfigureCtrl', ['$scope', '$route','$routeParams',
-  function($scope, $route, $routeParams){
+  function($scope, $route, $routeParams){ // http://goo.gl/oa3Kzq
     console.log("route", $route);
     console.log('ConfigureCtrl');
     this.params = $routeParams;
+    console.log("params", this.params);
 
-
+    $scope.register = function(){
+      console.log($scope.accountName);
+    }
 
 
   }]);
